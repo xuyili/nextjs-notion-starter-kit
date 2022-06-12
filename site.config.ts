a167c0575726c219f96615a07cf122343408190f
xuyili <35171974+xuyili@users.noreply.github.com>
2022-06-12 21:26:05 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '940f9ad263fb4a8dbef316f3e553f923',
+  rootNotionPageId: '9c5e9a8c9d9e4476a540ba77ada63cb5',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: 'Yili Notion Starter Kit',
-  domain: 'https://changeable-cook-a91.notion.site/940f9ad263fb4a8dbef316f3e553f923',
+  domain: 'https://xuyili.notion.site/940f9ad263fb4a8dbef316f3e553f923',
   author: 'Yili',
 
   // open graph metadata (optional)
